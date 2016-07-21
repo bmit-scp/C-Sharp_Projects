5f2e59fdd4f0bc49628312aca2cc6ced556c2d10
Patric S. <patric.schenk@espas>
2016-07-21 08:48:23 +0200

    Spiel_1_Jump v1.0

---
+++ Spiel _1_Jump/bforce/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace bforce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flappy = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.lblgame = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flappy
+            // 
+            this.flappy.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flappy.Location = new System.Drawing.Point(350, 240);
+            this.flappy.Name = "flappy";
+            this.flappy.Size = new System.Drawing.Size(20, 20);
+            this.flappy.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jump";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(557, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 91);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "5";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(488, 533);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(18, 16);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Location = new System.Drawing.Point(480, 536);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(29, 18);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Location = new System.Drawing.Point(480, 533);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(17, 21);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Location = new System.Drawing.Point(480, 533);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(24, 21);
+            this.panel4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 455);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 91);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(418, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "score:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Points";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(573, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "lives";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(497, 18);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(0, 25);
+            this.score.TabIndex = 11;
+            // 
+            // lblgame
+            // 
+            this.lblgame.AutoSize = true;
+            this.lblgame.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblgame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblgame.Location = new System.Drawing.Point(74, 188);
+            this.lblgame.Name = "lblgame";
+            this.lblgame.Size = new System.Drawing.Size(530, 91);
+            this.lblgame.TabIndex = 12;
+            this.lblgame.Text = "GAME OVER";
+            this.lblgame.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 555);
+            this.Controls.Add(this.lblgame);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flappy);
+            this.Name = "Form1";
+            this.Text = "la";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel flappy;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label lblgame;
+
+    }
+}
+
---
+++ Spiel _1_Jump/bforce/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bforce
+{
+    public partial class Form1 : Form
+    {
+        int bjump = 0;
+        int bzähler = 0;
+        int live = 5;
+        int ran = 0;
+        int zählerp = 0;
+        int score1 = 0;
+        int gameoverzähler = 0;
+        
+
+        // r random erstellen
+        static Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            newgame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+      
+
+
+            // flappy //
+            
+                flappy.Location = new Point(
+                flappy.Location.X, flappy.Location.Y +10);
+            
+            if (bjump == 1)
+            {
+      
+                flappy.Location = new Point(
+                flappy.Location.X, flappy.Location.Y -30);
+                bzähler = bzähler + 1;
+                if (bzähler == 5)
+                {
+                    bjump = 0;
+                    bzähler = 0;
+                }
+            }
+         
+            if (flappy.Location.Y > 500)
+            {
+                flappy.Location = new Point(
+                flappy.Location.X, flappy.Location.Y -10);      
+            }
+
+            // röhren
+
+            panel1.Location = new Point(
+            panel1.Location.X -10, panel1.Location.Y);
+            panel2.Location = new Point(
+            panel2.Location.X -10, panel2.Location.Y);
+            if (panel1.Location.X == 0)
+            {newpanel();}
+
+            panel3.Location = new Point(
+            panel3.Location.X - 10, panel3.Location.Y);
+            panel4.Location = new Point(
+            panel4.Location.X - 10, panel4.Location.Y);
+            if (panel1.Location.X == 300)
+            { newpanel2(); }
+   
+
+            //test
+
+            if (panel1.Location.X == 350 || panel3.Location.X == 350)
+            {
+                if ((flappy.Location.Y + 10) > ran &&
+                  (flappy.Location.Y + 10) < (ran + 200))
+                {
+
+                    label2.Text = Convert.ToString(zählerp);
+                    zählerp = zählerp + 1;
+                    if (zählerp >score1)
+                    {
+                        score1 = zählerp;
+                        score.Text = Convert.ToString(score1);
+                    }
+                }
+                else
+                {
+                    if (live >= 1)
+                    {
+                        live = live - 1;
+                        label1.Text = Convert.ToString(live);
+                    }
+
+
+                    if (live == 0)
+                    {
+                        lblgame.Visible = true;
+                        
+                        gameoverzähler = gameoverzähler + 1;
+                        if (gameoverzähler == 2)
+                        {
+                            lblgame.Visible = false;
+                            newgame();
+                            gameoverzähler = 0;
+                        }                
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+                bjump = 1;
+
+        }
+
+        private void newpanel()
+        {
+            ran = r.Next(0, 300);
+            panel1.Size = new Size(10, ran);
+            panel2.Size = new Size(10, (300 - ran));
+            panel1.Location = new Point(600, 0);
+            panel2.Location = new Point(600, (ran + 200));
+        }
+        private void newpanel2()
+        {
+            ran = r.Next(0, 300);
+            panel3.Size = new Size(10, ran);
+            panel4.Size = new Size(10, (300 - ran));
+            panel3.Location = new Point(600, 0);
+            panel4.Location = new Point(600, (ran + 200));
+        }
+        private void newgame()
+        {
+
+            timer1.Start();
+            newpanel();
+            live = 5;
+            zählerp = 0;
+            label2.Text = Convert.ToString(zählerp);
+        }
+
+    
+    }
+}
